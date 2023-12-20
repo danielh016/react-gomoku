57999c00c2e7bdecd06ef2fd2bfa503eb888a3cb
danielh016 <dan3388a@gmail.com>
2023-12-20 16:38:39 +1100

    Updates

---
+++ precache-manifest.cfb009b3c24bed6a8324a3f6b5992de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab4861a836306f39d1040a6c2edb37e",
+    "url": "/react-gomoku/index.html"
+  },
+  {
+    "revision": "3b13b3340af96308d0f9",
+    "url": "/react-gomoku/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d5e7e9ae8be87ad2a600",
+    "url": "/react-gomoku/static/js/2.2eb0d1b1.chunk.js"
+  },
+  {
+    "revision": "c9d95c37022699f4b97f62fb71120cee",
+    "url": "/react-gomoku/static/js/2.2eb0d1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b13b3340af96308d0f9",
+    "url": "/react-gomoku/static/js/main.0c51dcd7.chunk.js"
+  },
+  {
+    "revision": "7896df0a444d0501b721",
+    "url": "/react-gomoku/static/js/runtime-main.f0d3f762.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.d1373ff2f48ee27d7c4df2756b624806.js"
+  "/react-gomoku/precache-manifest.cfb009b3c24bed6a8324a3f6b5992de1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gomoku/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
