27347eccd16d562ec0ac39092eb08ae2c62322ac
danielh016 <dan3388a@gmail.com>
2023-12-20 16:21:12 +1100

    Updates

---
+++ precache-manifest.d1373ff2f48ee27d7c4df2756b624806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001f1ea61ebf1f61dc70e705afaf8e1b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a5512c888dff7fbe391a",
+    "url": "/react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "edcdbde122c18c9e6c18",
+    "url": "/react-gh-pages/static/js/2.f03f3ae3.chunk.js"
+  },
+  {
+    "revision": "c9d95c37022699f4b97f62fb71120cee",
+    "url": "/react-gh-pages/static/js/2.f03f3ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5512c888dff7fbe391a",
+    "url": "/react-gh-pages/static/js/main.d95162bd.chunk.js"
+  },
+  {
+    "revision": "13ed96409ba51e9c4216",
+    "url": "/react-gh-pages/static/js/runtime-main.51d7ea19.js"
+  }
+]);
